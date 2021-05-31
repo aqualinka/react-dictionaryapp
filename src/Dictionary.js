7bc17f66a7f09796309a9f2884574b99dcdf6f36
aqualinka <michaela.kovaja@gmail.com>
2021-05-31 11:24:53 +0100

    created search engine

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default function Dictionary(){
+    const[keyword,setKeyword]=useState(null);
+
+function search(event){
+    event.preventDefault();
+    alert(keyword);
+
+}
+function getInputValue(event){
+    setKeyword(event.target.value);
+}
+
     return (
     <div className="Dictionary">
-        <form>
-            <input type="search" className="form-control"/>
+        <form onSubmit={search}>
+            <input type="search" onChange={getInputValue} className="form-control"/>
         </form>
+        <div>
+            {keyword}
+        </div>
     </div>
     );
 }
