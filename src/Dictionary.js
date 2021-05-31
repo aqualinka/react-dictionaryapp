22b6c8283defcd38b20194e10579f726ed2a311b
aqualinka <michaela.kovaja@gmail.com>
2021-05-31 12:13:34 +0100

    added axios and api Url

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import axios from "axios";
 
 export default function Dictionary(){
     const[keyword,setKeyword]=useState(null);
@@ -11,6 +12,14 @@ function search(event){
 function getInputValue(event){
     setKeyword(event.target.value);
 }
+
+function handleResponse(response){
+    console.log(response.data[0]);
+}
+//https://dictionaryapi.dev/
+
+ let apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en_GB/${keyword}`;
+axios.get(apiUrl).then(handleResponse);
 
     return (
     <div className="Dictionary">
