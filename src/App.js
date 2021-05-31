2982ba1bc84124eb300518c406c8916d0cb201da
aqualinka <michaela.kovaja@gmail.com>
2021-05-31 10:32:03 +0100

    cleanup, added new component dictionary

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import hello from './dictionary.png';
+
 import './App.css';
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={hello} className="App-logo" alt="logo" />
-       
-      </header>
+      <div className="container mt-3 text-center">
+        <header className="App-header">
+          <h1>D.I.C.T.I.O.N.A.R.Y</h1>
+        </header>
+        <main>
+         <Dictionary />
+        </main>
+        <footer>
+          Coded By Mika <a href="">open-sourced on Github</a>
+        </footer>
+      </div>
+     
     </div>
   );
 }
