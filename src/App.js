93f5db3bfe36b78e3036ae6df29f96fbc53f6f74
aqualinka <michaela.kovaja@gmail.com>
2021-05-31 09:31:57 +0100

    clean-up, added foto

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import hello from './dictionary.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={hello} className="App-logo" alt="logo" />
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
