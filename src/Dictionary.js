dd52dd2d9cfb202c90062dcfef0e983c1cae9916
aqualinka <michaela.kovaja@gmail.com>
2021-06-01 11:13:41 +0100

    update various files

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,35 +1,18 @@
-import React, {useState} from "react";
-import axios from "axios";
-import Results from "./Results";
+import React from "react";
 
 export default function Dictionary(){
-    const[keyword,setKeyword]=useState(null);
-    const[results,setResults]=useState(null);
-
-function search(event){
-    event.preventDefault();
-
+function submitForm(event){
+    event.preventDefault();  
+    alert("submitted");
 }
-function getInputValue(event){
-    setKeyword(event.target.value);
+function changeInputValue(event){
+    console.log(event.target.value);
 }
 
-function handleResponse(response){
- setResults(response.data[0]);
- console.log(response.data[0]);
-}
-
-//https://dictionaryapi.dev/
-
- let apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en_GB/${keyword}`;
-axios.get(apiUrl).then(handleResponse);
-
-    return (
-    <div className="Dictionary">
-        <form onSubmit={search}>
-            <input type="search" onChange={getInputValue} className="form-control"/>
+    return (<div className="Dictionary">
+        <form onSubmit={submitForm}>
+        <input onChange={changeInputValue} type="text" autoFocus={true} className="form-control"/>
         </form>
-        <Results results={results}/>
-    </div>
-    );
+
+    </div>);
 }
