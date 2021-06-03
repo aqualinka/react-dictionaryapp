63b4bee30db6d13190d2077a3f01fa7e3a532e7d
aqualinka <michaela.kovaja@gmail.com>
2021-06-03 20:03:24 +0100

    added github icon

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Dictionary from "./Dictionary";
 import './App.css';
+import {FaGithub} from "react-icons/fa";
 
 function App() {
 
@@ -8,13 +9,16 @@ function App() {
     <div className="App">
       <div className="container my-5">
         <header className="App-header">
+        
           <h1 className="text-center">D.I.C.T.I.O.N.A.R.Y</h1>
         </header>
         <main>
           <Dictionary defaultKeyword="coding"/>
         </main>
         <footer className="text-center">
-          Coded By Mika | <a href="https://github.com/aqualinka/react-dictionaryapp" target="_blank" rel="noopener noreferrer">open-sourced on Github</a>| hosted on <a href="https://relaxed-shockley-3c677b.netlify.app/">Netlify</a>
+
+          Coded By Mika | <a href="https://github.com/aqualinka/react-dictionaryapp" target="_blank" rel="noopener noreferrer">
+          <FaGithub /> </a>| hosted on <a href="https://relaxed-shockley-3c677b.netlify.app/">Netlify</a>
         </footer>
       </div>
      
